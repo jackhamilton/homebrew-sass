e792af8345e4621e41e2cc520489ce277c99fb8d
Jack Hamilton <jackham800@gmail.com>
2025-02-27 23:16:08 -0600

    update

--- Formula/sass.rb
+++ Formula/sass.rb
@@ -1,8 +1,8 @@
 class Sass < Formula
   desc "XCode assistant tool written in Rust"
-  homepage "https://github.com/jackhamilton/sass-rs"
+  homepage "https://github.com/jackhamilton/sass-rs/"
   url "https://github.com/jackhamilton/sass-rs/releases/download/0.1.0/sass-mac.tar.gz"
-  sha256 "c49efa8a318bdfd0f36cffacb9538f805065d36322efe4a6b326f23b516de52f"
+  sha256 "682f6645b0145aaf68be4d5ecdb23d6ccfda8acafe79efd1cda6dac444290a26"
   version "0.1.0"
 
   def install
