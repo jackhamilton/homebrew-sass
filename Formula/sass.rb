13b8f206e225def574b79e88eb3010f39602a27b
Jack Hamilton <jackham800@gmail.com>
2025-03-04 19:01:54 -0600

    sass-rs 0.2.2
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/sass.rb
+++ Formula/sass.rb
@@ -1,9 +1,9 @@
 class Sass < Formula
   desc "XCode assistant tool written in Rust"
   homepage "https://github.com/jackhamilton/sass-rs/"
-  url "https://github.com/jackhamilton/sass-rs/archive/refs/tags/0.2.1.tar.gz"
-  sha256 "14a70cd610a5ae5fba6ebb2aea9664a4f429f1614c745d027264bb60517a5655"
-  version "0.2.1"
+  url "https://github.com/jackhamilton/sass-rs/archive/refs/tags/0.2.2.tar.gz"
+  sha256 "93b356614e68b0e33b1d10f5b5f2800f34c12e057a2145696aafed2727da74bc"
+  version "0.2.2"
 
   def install
     bin.install "sass"
